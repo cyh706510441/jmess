d0aa8e1c434ed0c1e9bc7b0ed8ebfb42d53e4ce7
yuhang.chen <yuhang.chen@beibei.com>
2020-05-08 17:38:13 +0800

    update

--- dubbo-demo/dubbo-demo-provider/src/main/java/org/example/provider/Provider.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/org/example/provider/Provider.java
@@ -8,10 +8,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Provider {
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"classpath:provider.xml"});
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext(new String[] {"classpath:provider.xml"});
         context.start();
 
-        // 按任意键退出
         System.in.read();
     }
 }
